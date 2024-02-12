2b6613aa2e183e10ec5fc567924ddfe29af6710a
mercy-kantai <kantaiemilymercy@gmail.com>
2024-02-12 10:21:30 +0300

    added one file each

---
+++ Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+ var name = "Mercy Kantai"
+    println("name")
+    var age = 20
+    println("age")
+    var phoneNumber = 113912600
+     println("phoneNumber")
+    var weight = 70.0F
+    println("weight")
+    var nationality = "Kenyan"
+    println("nationality")
+
+}
